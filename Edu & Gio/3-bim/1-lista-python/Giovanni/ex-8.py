76a96c0cad9008c22831fed2ea90eaad63560971
Giovanni Rovesta <giovannirovesta@gmail.com>
2026-08-25 12:23:18 -0300

    Update ex-8.py

--- Edu & Gio/3-bim/1-lista-python/Giovanni/ex-8.py
+++ Edu & Gio/3-bim/1-lista-python/Giovanni/ex-8.py
@@ -1,17 +1,16 @@
-contador = 1
-positivos = 0
-negativos = 0
-zeros = 0
+div = 1
+qdiv = 0
 
-while contador <= 10:
-    n = int(input(f'Digite o {contador} número: '))
-    if n > 0:
-        positivos += 1
-    elif n < 0:
-        negativos += 1
+n = int(input('Digite um número inteiro positivo: '))
+
+if n > 0:
+    while div <= n:
+        if n % div == 0:
+            qdiv += 1
+        div += 1
+    if qdiv == 2:
+        print(f'O número {n} é primo')
     else:
-        zeros += 1
-    contador += 1
-print(f'Quantidade de números positivos: {positivos} \n')
-print(f'Quantidade de números negativos: {negativos} \n')
-print(f'Quantidade de zeros: {zeros}')
+        print(f'O número {n} não é primo')
+else:
+    print('O número é inválido')
