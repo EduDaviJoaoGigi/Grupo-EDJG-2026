4fbaa1a7fdcdb4bf51a69d4a5abbd8c6ac779c0e
Giovanni Rovesta <giovannirovesta@gmail.com>
2026-09-01 08:52:41 -0300

    Delete Edu & Gio/3-bim/1-lista-python/Giovanni directory

--- Edu & Gio/3-bim/1-lista-python/Giovanni/ex-3.py
+++
@@ -1,9 +0,0 @@
-n = int(input('Escolha um número inteiro: '))
-contador = 1
-
-if n > 0:
-    while contador <= n:
-        print(f'{contador}')
-        contador += 1
-else:
-    print('Seu número é invalido')
